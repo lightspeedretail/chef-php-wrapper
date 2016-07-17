75de54e0b14535e38fc813a8b7ee378d98e76a52
Jonathan Serafini <jonathan@serafini.ca>
2016-07-17 22:10:50 +0200

    Bugfix release

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jonathan@serafini.ca'
 license 'apachev2'
 description 'Installs/Configures php_wrapper'
 long_description 'Installs/Configures php_wrapper'
-version '2.1.0'
+version '2.1.1'
 
 depends "common_attrs" # resource.load_properties support
 depends "php", "~> 1.7"
