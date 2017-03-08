e879e4eb692a875a2213a73076873cab356d9de2
Michiel Rensen <michiel.rensen@lightspeedhq.com>
2017-03-08 14:46:50 +0100

    Bumped composer version to v1.3.3

--- attributes/php-composer.rb
+++ attributes/php-composer.rb
@@ -1,5 +1,5 @@
 default['php']['composer'].tap do |config|
   config['url'] = 'https://getcomposer.org/installer'
   config['command'] = '/usr/local/bin/composer'
-  config['version'] = '1.0.0-alpha10'
+  config['version'] = '1.3.3'
 end
